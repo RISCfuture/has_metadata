87b7a60a68a85e2492048373863b079af93457c9
Tim Morgan <git@timothymorgan.info>
2012-01-23 18:00:12 -0800

    Revert "Rails 3.1 migration format"
    This reverts commit 9cb7a54c90c7412bf14c0405d0d482e42f1cc4de.

--- templates/create_metadata.rb
+++ templates/create_metadata.rb
@@ -1,7 +1,11 @@
 class CreateMetadata < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :metadata do |t|
       t.text :data
     end
+  end
+
+  def self.down
+    drop_table :metadatas
   end
 end
